89afeda80fbe0c8bcfb5614722016a9fff9204d7
abdullmajeed1380 <a.majeed.1380@gmail.com>
2026-06-26 08:38:50 +0330

    Update and rename firebase.ts to firebase.ts

---
+++ firebase.ts
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDYiwHhAs18MUsAV6CZ5_vxqQoabQXvmhM",
+    authDomain: "jazirah-fbac6.firebaseapp.com",
+    projectId: "jazirah-fbac6",
+    storageBucket: "jazirah-fbac6.firebasestorage.app",
+    messagingSenderId: "1082545229755",
+    appId: "1:1082545229755:web:5e2aefd15ba71e00e5be9e"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
